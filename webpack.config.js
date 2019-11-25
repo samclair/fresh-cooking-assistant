6eaef4d426861d987c6835986c7ab68d1f88144a
Christian <christianparizeau@gmail.com>
2019-11-25 10:56:52 -0800

    initial skeleton including npm package dependencies php.ini and npmrc

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const clientPath = path.resolve(__dirname, 'client');
+const publicPath = path.resolve(__dirname, 'server/public');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: clientPath,
+  output: {
+    path: publicPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: clientPath,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    contentBase: publicPath,
+    historyApiFallback: true,
+    watchContentBase: true,
+    stats: 'minimal',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9000'
+      }
+    }
+  }
+};
