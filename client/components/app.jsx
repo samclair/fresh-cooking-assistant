ee77fa25382b064667a1eb86ff500bc4182e5b19
Doctorbingbing <christianparizeau@gmail.com>
2019-11-26 17:58:09 +0000

    initial workings for routing on the front end

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,19 @@
 import React from 'react';
+import Landing from './landing';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+function Login(props) {
+  return <h2>This is a login Page</h2>;
+}
+
+function Placeholder(props) {
+  return <h2>This is a placeholder page</h2>;
+}
 
 export default class App extends React.Component {
   constructor(props) {
@@ -18,8 +33,38 @@ export default class App extends React.Component {
   }
 
   render() {
-    return this.state.isTesting
-      ? <h1>Testing connections...</h1>
-      : <h1>{ this.state.message }</h1>;
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Landing</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/placeholder">Placeholder</Link>
+            </li>
+            <Switch>
+              <Route path="/">
+                <Landing text="Landing Page" />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/placeholder">
+                <Placeholder />
+              </Route>
+            </Switch>
+
+          </ul>
+        </div>
+      </Router>
+    );
+
+    // return this.state.isTesting
+    //   ? <h1>Testing connections...</h1>
+    //   : <h1>{this.state.message}</h1>;
   }
 }
