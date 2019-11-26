8d7590169ee9187bdb6792a1cafcb910eb227993
Christian Haney <christianhaney1996@gmail.com>
2019-11-26 15:56:08 -0800

    Fresh! | Backend - Christian Haney (endpoints) created get_produce_seasonality fucntion in produce-details endpoint function queries database to get the start and end dates for each season corresponding to the provided produceId an error is thrown if no results are found created function check_in_season in produce-details endpoint function gets current time and checks if it falls in any of the ranges of dates provided function returns true if there is a match and false if there is none produce-details endpoint now returns an associative array with produce details and a boolean isInSeason

--- server/api/produce-details.php
+++ server/api/produce-details.php
@@ -6,7 +6,13 @@ if ($request['method'] === 'GET') {
   $produce_id = $request['query']['produceId'];
   $produce_id = intval($produce_id);
   if ($produce_id <= 0) {throw new ApiError('Valid Produce ID required', 400); }
-  $response['body'] = 'Hello There!';
+  $produce_details = get_produce_details($link, $produce_id);
+  $seasonality_dates = get_produce_seasonality($link, $produce_id);
+  $is_in_season = check_in_season($seasonality_dates);
+  $response['body'] = [
+    details => $produce_details,
+    isInSeason => $is_in_season
+  ];
   send($response);
 }
 
@@ -20,4 +26,30 @@ function get_produce_details($link, $produce_id) {
   if (!mysqli_num_rows($result)) {throw new ApiError('Produce details not found', 404); }
   $produce = mysqli_fetch_assoc($result);
   return $produce;
+}
+
+function get_produce_seasonality($link, $produce_id) {
+  $sql = "
+    SELECT `startDate`, `endDate`
+    FROM `seasons`
+    JOIN `produceSeasons`
+      ON `seasons`.`id` = `produceSeasons`.`seasonId`
+    WHERE `produceId` = $produce_id
+  ";
+  $result = mysqli_query($link, $sql);
+  if (!mysqli_num_rows($result)) {throw new ApiError('Seasonal data not found', 404); }
+  $seasonality_dates = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  return $seasonality_dates;
+}
+
+function check_in_season($seasonality_dates) {
+  $now = time();
+  foreach ($seasonality_dates as $start_end) {
+    $start_date = strtotime($start_end['startDate']);
+    $end_date = strtotime($start_end['endDate']);
+    if ($now >= $start_date && $now <= $end_date ) {
+      return true;
+    }
+  }
+  return false;
 }
