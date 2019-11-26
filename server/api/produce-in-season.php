97c6322fd1dd3619c8cf2b779d271fb17b3595c6
Christian Haney <christianhaney1996@gmail.com>
2019-11-26 12:29:07 -0800

    Fresh! | Backend - Christian Haney (endpoints) created check_produce_in_season function in produce-in-season endpoint function sends query to database for all produce names and ids that correspond to the seasonId from the request params if no produce in season the function returns an empty array otherwise an array of objects containing the produceId and name is returned modified GET conditional to call check_produce_in_season modified GET conditional to set the result of the call in the response body

--- server/api/produce-in-season.php
+++ server/api/produce-in-season.php
@@ -3,14 +3,23 @@
 $link = get_db_link();
 
 if ($request['method'] === 'GET') {
-  if(!isset($request['query']['seasonId'])) {
-    throw new ApiError('Valid Season ID required', 400);
-  }
-  $season_id = $request['query']['seasonId'];
-  $response['body'] = 'Hello there!';
+  $season_id = ($request['query']['seasonId']);
+  $season_id = intval($season_id);
+  if($season_id <= 0) { throw new ApiError('Valid Season ID required', 400); }
+  $response['body'] = check_produce_in_season($link, $season_id);
   send($response);
 }
 
 function check_produce_in_season($link, $season_id) {
-  return null;
+  $sql = "
+    SELECT `produce`.`id`, `name`
+    FROM `produce`
+    JOIN `produceSeasons`
+      ON `produce`.`id` = `produceSeasons`.`produceId`
+    WHERE `seasonId` = $season_id
+    ";
+  $result = mysqli_query($link, $sql);
+  if (!mysqli_num_rows($result)) { $produce = []; }
+  else { $produce = mysqli_fetch_all($result, MYSQLI_ASSOC); }
+  return $produce;
 }
