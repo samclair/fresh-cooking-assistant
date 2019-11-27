1063385859ebb445fece1b147ce96c4e1236dc44
kittenbites <sammie.nagle@gmail.com>
2019-11-27 15:14:19 -0800

    Converted Seasons component to react class component added methods to fetch season data from API endpoint updates state according to data received from the database

--- client/components/seasons.jsx
+++ client/components/seasons.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Seasons(props) {
-  return (
-    <div>
-      <h1>This is the Seasons Page</h1>
-      <ul>
-        <li>
-          <Link to={'season/Spring'}>Spring</Link>
-        </li>
-        <li>
-          <Link to={'season/Summer'}>Summer</Link>
-        </li>
-        <li>
-          <Link to={'season/Fall'}>Fall</Link>
-        </li>
-        <li>
-          <Link to={'season/Winter'}>Winter</Link>
-        </li>
-      </ul>
-      <h3>Please select a season</h3>
-    </div>
-  );
+class Seasons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSeason: '',
+      seasonList: []
+    };
+  }
+
+  getAllSeasons() {
+    fetch('api/seasons')
+      .then(result => result.json())
+      .then(seasonList => this.setState({ currentSeason: '', seasonList: [] }))
+      .catch(error => console.error(error.message));
+  }
+
+  componentDidMount() {
+    this.getAllSeasons();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is the Seasons Page</h1>
+        <ul>
+          <li>
+            <Link to={'season/Spring'}>Spring</Link>
+          </li>
+          <li>
+            <Link to={'season/Summer'}>Summer</Link>
+          </li>
+          <li>
+            <Link to={'season/Fall'}>Fall</Link>
+          </li>
+          <li>
+            <Link to={'season/Winter'}>Winter</Link>
+          </li>
+        </ul>
+        <h3>Please select a season</h3>
+      </div>
+    );
+  }
 }
 export default Seasons;
