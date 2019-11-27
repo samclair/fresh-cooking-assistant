e65e976b9e025d16e55e418718ba4fc269556d49
Doctorbingbing <christianparizeau@gmail.com>
2019-11-27 23:01:40 +0000

    inSeason badge is now rendered dynamically

--- client/components/produce-details.jsx
+++ client/components/produce-details.jsx
@@ -22,23 +22,31 @@ class ProduceDetails extends React.Component {
   }
 
   render() {
-    const details = this.state.details;
+    const { selection, storage, nutrition, produceImg } = this.state.details;
+    let isInSeasonBadge = null;
+    if (this.state.isInSeason) {
+      isInSeasonBadge = <div className="badge primary-label"><h2>In season now!</h2></div>;
+    }
+
     return (
       <div>
         <div className="header-container">
-          <img src="./images/acornsquash.jpg" alt="an acorn squash" className="header-img" />
-          <div className="badge primary-label"><h2>In season now!</h2></div>
-          <div className="badge primary-label"><h2>Add to Cart</h2></div>
+          <img src={produceImg} alt={this.name} className="header-img" />
+          {isInSeasonBadge}
+          {/* <div className="badge primary-label"><h2>Add to Cart</h2></div> */}
           <h1 className="green">{this.name}</h1>
         </div>
         <div className="container">
           <h2 className="yellow">Selection</h2>
-          <div className="body-text">{details.selection}
+          <div className="body-text">{selection}
           </div>
           <h2 className="yellow">Storage</h2>
-          <div className="body-text">{details.storage}</div>
+          <div className="body-text">{storage}
+          </div>
+          <h2 className="yellow">Nutrition</h2>
+          <div className="body-text">{nutrition}</div>
         </div>
-      </div>);
+      </div >);
   }
 }
 
