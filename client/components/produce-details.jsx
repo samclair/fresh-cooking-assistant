f13333bc54d6c45c451e8e47094c8846b2b9d3cb
Doctorbingbing <christianparizeau@gmail.com>
2019-11-27 21:24:13 +0000

    storage and selection fields and name now dynamically populate from fetch requests

--- client/components/produce-details.jsx
+++ client/components/produce-details.jsx
@@ -22,20 +22,21 @@ class ProduceDetails extends React.Component {
   }
 
   render() {
+    const details = this.state.details;
     return (
       <div>
         <div className="header-container">
           <img src="./images/acornsquash.jpg" alt="an acorn squash" className="header-img" />
           <div className="badge primary-label"><h2>In season now!</h2></div>
           <div className="badge primary-label"><h2>Add to Cart</h2></div>
-          <h1 className="green">Acorn Squash</h1>
+          <h1 className="green">{this.name}</h1>
         </div>
         <div className="container">
           <h2 className="yellow">Selection</h2>
-          <div className="body-text">Select acorn squash that are dull and heavy for their size. Avoid squash with soft spots or cracks.
+          <div className="body-text">{details.selection}
           </div>
           <h2 className="yellow">Storage</h2>
-          <div className="body-text">Store in da freezer</div>
+          <div className="body-text">{details.storage}</div>
         </div>
       </div>);
   }
