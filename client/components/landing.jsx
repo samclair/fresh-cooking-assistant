dbd9e670474295d01bdda1a7699d9747846275ac
Doctorbingbing <christianparizeau@gmail.com>
2019-11-27 23:32:45 +0000

    landing page is now a stateful component landing page now gets current season from the database

--- client/components/landing.jsx
+++ client/components/landing.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
-function Landing(props) {
-  return <h1>{props.text}</h1>;
+class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentSeason: null };
+  }
+
+  getCurrentSeason() {
+    fetch('/api/seasons')
+      .then(res => res.json())
+      .then(({ currentSeason }) => this.setState({ currentSeason }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Landing Page</h1>
+        <h2>It is currently {this.state.currentSeason}</h2>
+      </div>
+    );
+  }
 }
 
 export default Landing;
