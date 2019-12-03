b92ed5bfa586baabbb11e5ee09503b925eec96c5
Doctorbingbing <christianparizeau@gmail.com>
2019-12-03 18:05:33 +0000

    touched up styling on navbar

--- client/components/nav-menu.jsx
+++ client/components/nav-menu.jsx
@@ -19,23 +19,23 @@ class NavMenu extends React.Component {
   render() {
     const openMenu = (
       <div>
-        <nav className="navbar mb-2">
+        <nav className="navbar">
           <a className="navbar-brand title" style={{ color: 'white', fontSize: '22px' }}>FRESH!</a>
           <i onClick={this.swapView} className="fas fa-bars fa-lg py-2"></i>
         </nav>
         <div className='page-overlay' onClick={this.swapView}></div>
         <div className="menu-overlay">
-          <div className='py-2 d-flex' style={{ borderBottom: '1px solid lightgray' }}>
-            <i onClick={this.swapView} className = 'fas fa-arrow-circle-left fa-2x green align-self-center ml-2' style = {{ position: 'absolute' }} ></i>
-            <span className = 'green font-rubik h2 m-0' style = {{ display: 'inline-block', textAlign: 'center', width: '100%' }}>MENU</span>
+          <div className='py-2 mb-2 d-flex' style={{ borderBottom: '1px solid lightgray' }}>
+            <i onClick={this.swapView} className='fas fa-arrow-circle-left fa-2x green align-self-center ml-2' style={{ position: 'absolute' }} ></i>
+            <span className='green font-rubik h2 m-0' style={{ display: 'inline-block', textAlign: 'center', width: '100%' }}>MENU</span>
           </div>
-          <ul className = 'body-text'>
+          <ul className='body-text'>
             <li onClick={this.swapView} className='mb-3'>
               <h2><Link to="/" className='body-text'>Landing</Link></h2>
             </li>
             {this.state.menuOptions.map((navLink, index) => {
               return (
-                <li key={index} className= 'mb-3'onClick={this.swapView}>
+                <li key={index} className='mb-3' onClick={this.swapView}>
                   <h2><Link to={`/${navLink.toLowerCase()}`} className='body-text'>{navLink}</Link></h2>
                 </li>
               );
@@ -47,8 +47,8 @@ class NavMenu extends React.Component {
     );
 
     const closedMenu = (
-      <nav className="navbar mb-2 sticky-top">
-        <a className="navbar-brand title"><Link style={{ color: 'white', fontSize: '22px' }} to="/">FRESH!</Link></a>
+      <nav className="navbar sticky-top">
+        <span className="navbar-brand title"><Link style={{ color: 'white', fontSize: '22px' }} to="/">FRESH!</Link></span>
 
         <i onClick={this.swapView} className="fas fa-bars fa-lg py-2"></i>
       </nav>
