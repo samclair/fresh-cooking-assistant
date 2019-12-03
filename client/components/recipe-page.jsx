c7fdb6fae4dad18b54b5ed4966c097c034e3aa3f
Doctorbingbing <christianparizeau@gmail.com>
2019-12-03 22:41:42 +0000

    recipe page template setup

---
+++ client/components/recipe-page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RecipePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+  }
+
+  render() {
+    return <div>This is a Recipe Page</div>;
+  }
+}
+
+export default RecipePage;
