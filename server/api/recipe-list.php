9ae501683b7136805d83772ad09338e96107bc73
Christian Haney <christianhaney1996@gmail.com>
2019-12-03 17:36:18 -0800

    Fresh! | Recipe List Endpoint - Christian Haney (backend) modifed recipe-list.php endpoint to return an array of recipe objects containing only name, id, and thumbnail img url

--- server/api/recipe-list.php
+++ server/api/recipe-list.php
@@ -6,11 +6,19 @@ require_once '_helpers.php';
 if ($request['method'] === 'GET') {
   $tags = $request['query']['tags'];
   if (!isset($tags)) { throw new ApiError("Search tag required", 400); }
-  $response['body'] = get_recipe_list($tags, $count, $tasty_api_key);
+  $results = get_recipe_list($tags, $tasty_api_key);
+  $response['body'] = [];
+  foreach ($results -> results as $recipe) {
+    array_push($response['body'], [
+      'id' => $recipe -> canonical_id,
+      'name' => $recipe -> name,
+      'thumbnail' => $recipe -> thumbnail_url
+    ]);
+  }
   send($response);
 }
 
-function get_recipe_list($tags, $count, $tasty_api_key) {
+function get_recipe_list($tags, $tasty_api_key) {
   $ch = curl_init();
   $options = [
     CURLOPT_URL => "https://tasty.p.rapidapi.com/recipes/list?q=$tags",
@@ -24,8 +32,8 @@ function get_recipe_list($tags, $count, $tasty_api_key) {
     CURLOPT_TIMEOUT => 10
   ];
   curl_setopt_array($ch, $options);
-  $result = curl_exec($ch);
+  $results = curl_exec($ch);
   $error = curl_error($ch);
   curl_close($ch);
-  return $error ? $error : json_decode($result);
+  return $error ? $error : json_decode($results);
 }
