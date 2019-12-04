4e68ab76116d60f9d6dcbabd74708a3d00167eda
Christian Haney <christianhaney1996@gmail.com>
2019-12-03 17:02:29 -0800

    Fresh! | Recipe List Endpoint - Christian Haney (backend) modified recipe-list.php endpoint to no longer expect a count param created get_recipe_list function get_recipe_list_function executes a cURL call to the tasty api with the provided tags param get_recipe_list returns the error if the call fails or the JSON decoded result of the call endpoint returns the result of the function call

--- server/api/recipe-list.php
+++ server/api/recipe-list.php
@@ -5,7 +5,27 @@ require_once '_helpers.php';
 
 if ($request['method'] === 'GET') {
   $tags = $request['query']['tags'];
-  $sizes = intval($request['query']['sizes']);
   if (!isset($tags)) { throw new ApiError("Search tag required", 400); }
-  if (!isset($sizes) || $sizes < 0) { $sizes = 1; }
+  $response['body'] = get_recipe_list($tags, $count, $tasty_api_key);
+  send($response);
+}
+
+function get_recipe_list($tags, $count, $tasty_api_key) {
+  $ch = curl_init();
+  $options = [
+    CURLOPT_URL => "https://tasty.p.rapidapi.com/recipes/list?q=$tags",
+    CURLOPT_HTTPGET => true,
+    CURLOPT_HTTPHEADER => [
+      'x-rapidapi-host: tasty.p.rapidapi.com',
+      "x-rapidapi-key: $tasty_api_key"
+    ],
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_CONNECTTIMEOUT => 10,
+    CURLOPT_TIMEOUT => 10
+  ];
+  curl_setopt_array($ch, $options);
+  $result = curl_exec($ch);
+  $error = curl_error($ch);
+  curl_close($ch);
+  return $error ? $error : json_decode($result);
 }
