63d88716c0b1e4459ceb114fca6d546473998ce4
Doctorbingbing <christianparizeau@gmail.com>
2019-12-04 00:49:27 +0000

    added basic styling and structure to individual recipe pages

--- client/components/recipe-page.jsx
+++ client/components/recipe-page.jsx
@@ -16,7 +16,28 @@ class RecipePage extends React.Component {
   }
 
   render() {
-    return <div>This is a Recipe Page Recipe # {this.id}</div>;
+    return (
+      <div>
+        <div className="header-image text-center">Here Goes a Hero Image</div>
+        <div className="container">
+          <div className='green text-center font-rubik my-3'>Recipe # {this.id}</div>
+          <div className="yellow">Ingredients</div>
+          <ul >
+            <li><u>Some</u></li>
+            <li><u>Ingredients</u></li>
+            <li><u>Go</u></li>
+            <li><u>Here</u></li>
+          </ul>
+          <div className="yellow">Directions</div>
+          <ol>
+            <li>Oven @ 350</li>
+            <li>Second Step</li>
+            <li>????</li>
+            <li>Profit</li>
+            <li>Never turn your oven off</li>
+          </ol>
+        </div>
+      </div>);
   }
 }
 
