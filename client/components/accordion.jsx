a594c82b4e67d2ca4dda21235f8040ddb60af5a0
Doctorbingbing <christianparizeau@gmail.com>
2019-12-05 00:08:08 +0000

    added accodion element user can now navigate through sub accordion menus conditional checking in accordion menu

--- client/components/accordion.jsx
+++ client/components/accordion.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Accordion(props) {
+class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { expand: false };
+    this.linkName = props.to ? props.to : 'Home';
+    this.expand = this.expand.bind(this);
+    this.subNavElems = props.subElems.map(element => {
+      let linkName = null;
+      if (element === 'Seasonal') {
+        linkName = '/recipes';
+      } else {
+        linkName = element;
+      }
+      return (<li className='p my-2' onClick={props.close} key={element}>
+        <Link to={linkName}><u>{element}</u></Link>
+      </li>);
+    });
+  }
 
-  return <div>{props.text}</div>;
+  expand() {
+    this.setState({ expand: !this.state.expand });
+  }
 
+  camelCase(text) {
+    if (!text) return '';
+    const firstChar = text[0].toUpperCase();
+    const restOfString = text.slice(1, text.length);
+    return firstChar + restOfString;
+  }
+
+  render() {
+    if (this.state.expand) {
+      return (
+        <div onClick={this.expand}>
+          <h2>{this.camelCase(this.linkName)}</h2>
+          <ul>
+            {this.subNavElems}
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <div onClick={this.expand}>
+          <h2>{this.camelCase(this.linkName)}</h2>
+        </div >);
+    }
+
+  }
 }
 
 export default Accordion;
--- client/components/nav-menu.jsx
+++ client/components/nav-menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Accordion from './accordion';
 // import Accordion from './accordion';
 
 class NavMenu extends React.Component {
@@ -8,22 +9,42 @@ class NavMenu extends React.Component {
     this.state = {
       display: false,
       menuOptions: ['', 'Login', 'Seasons', 'Recipes']
+
     };
+    this.navElements = null;
+    this.recipesSubnav = ['Seasonal', 'My Starred'];
     this.swapView = this.swapView.bind(this);
+
+  }
+
+  makeNavElements() {
+    this.navElements = (
+      <ul>
+        <li className='my-2'>
+          <Link className='h2' onClick={this.swapView} to=''>Home</Link>
+        </li>
+        <li className='my-2'>
+          <Link className='h2' onClick={this.swapView} to='/login'>Login</Link>
+        </li>
+        <li className='my-2'>
+          <Link className='h2' onClick={this.swapView} to='/seasons'>Seasons</Link>
+        </li>
+        <li className='my-2'>
+          <Accordion to='Recipes' close={this.swapView} subElems={this.recipesSubnav} />
+        </li>
+      </ul>
+    );
   }
 
   swapView() {
     this.setState({ display: !this.state.display });
   }
 
+  componentDidMount() {
+    this.makeNavElements();
+  }
+
   render() {
-    const menuOptions = this.state.menuOptions.map((navLink, index) => (
-      <li key={index} className='my-4' onClick={this.swapView}>
-        <Link to={`/${navLink.toLowerCase()}`} className='h2'>
-          {(navLink === '') ? 'Home' : navLink}
-        </Link>
-      </li>
-    ));
     const openMenu = (
       <div>
         <nav className="navbar">
@@ -36,7 +57,7 @@ class NavMenu extends React.Component {
             <i onClick={this.swapView} className='fas fa-arrow-circle-left fa-2x green mx-2' />
             <span className='green font-rubik h2 m-0'>MENU</span>
           </div>
-          <ul>{menuOptions}</ul>
+          {this.navElements}
         </div>
       </div>
     );
