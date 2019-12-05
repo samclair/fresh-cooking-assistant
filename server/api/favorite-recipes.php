d4ebf49bc3dfcc374b9d75073ae3251c3684b7f9
kittenbites <sammie.nagle@gmail.com>
2019-12-05 15:36:04 -0800

    Implemented proper function call & response body for adding recipe to favorites, set up initial DELETE method handling

--- server/api/favorite-recipes.php
+++ server/api/favorite-recipes.php
@@ -3,6 +3,8 @@
 require_once '_helpers.php';
 require_once '_api-keys.php';
 
+$link = get_db_link();
+
 if($request['method'] === 'POST'){
   $body_is_valid = isset($request['body']['recipeId']) &&
     isset($request['body']['recipeName']) &&
@@ -12,10 +14,19 @@ if($request['method'] === 'POST'){
   }
   $response['body'] = [
     'recipeId' => $request['body']['recipeId'],
-    'recipeName' => $request['body']['recipeName'],
-    'recipeImage' => $request['body']['recipeImage']
+    'isFavorited' => add_favorite_recipe(
+      $link,
+      $request['body']['recipeId'],
+      $request['body']['recipeName'],
+      $request['body']['recipeImage']
+      )
   ];
   send($response);
+}
+elseif($request['method'] === 'DELETE'){
+  if(!isset($request['body']['recipeId'])){
+    throw new ApiError("Recipe Id is required");
+  }
 }
 
 function add_favorite_recipe($link, $recipe_id, $name, $image){
@@ -24,4 +35,5 @@ function add_favorite_recipe($link, $recipe_id, $name, $image){
   VALUES
   ({$_SESSION['user_id']}, $recipe_id, $name, $image)";
   mysqli_query($link, $sql);
+  return true;
 }
