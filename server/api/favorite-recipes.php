25bc3a8e1adfd2881532d4dffb0f71b4eceea892
kittenbites <sammie.nagle@gmail.com>
2019-12-05 15:50:43 -0800

    Implemented query to database to delete a recipe a user has favorited

--- server/api/favorite-recipes.php
+++ server/api/favorite-recipes.php
@@ -27,13 +27,26 @@ elseif($request['method'] === 'DELETE'){
   if(!isset($request['body']['recipeId'])){
     throw new ApiError("Recipe Id is required");
   }
+  $response['body'] = [
+    'recipeId' => $request['body']['recipeId'],
+    'isFavorited' => unfavorite_recipe($link, $request['body']['recipeId'])
+  ];
+  send($response);
 }
 
 function add_favorite_recipe($link, $recipe_id, $name, $image){
-  $sql = "INSERT INTO `favoriteProduce`
+  $sql = "INSERT INTO `favoriteRecipe`
   (`userId`,`recipeId`,`name`,`image`)
   VALUES
   ({$_SESSION['user_id']}, $recipe_id, $name, $image)";
   mysqli_query($link, $sql);
   return true;
+}
+
+function unfavorite_recipe($link,$recipe_id){
+  $sql = "DELETE * FROM `favoriteRecipe`
+  WHERE `favoriteRecipe`.`recipeId` = $recipe_id
+  AND `favoriteRecipe`.`userId` = {$_SESSION['user_id']}";
+  mysqli_query($link, $sql);
+  return false;
 }
