8e969ebdb8feff011e0d81803f1c8a47f1bb1209
kittenbites <sammie.nagle@gmail.com>
2019-12-05 15:30:02 -0800

    implemented POST method for adding a recipe to a users favorite recipes post takes recipe id, name, and image and inserts new row into table based on user id from session

--- server/api/favorite-recipes.php
+++ server/api/favorite-recipes.php
@@ -1,24 +1,27 @@
-
 <?php
 
 require_once '_helpers.php';
 require_once '_api-keys.php';
 
 if($request['method'] === 'POST'){
-  $body_is_valid = isset($request['body']['userId']) &&
-    isset($request['body']['recipeId']) &&
+  $body_is_valid = isset($request['body']['recipeId']) &&
     isset($request['body']['recipeName']) &&
     isset($request['body']['recipeImage']);
-
   if(!$body_is_valid){
     throw new ApiError("Missing field from request body",400);
   }
   $response['body'] = [
-    'userId' =>  $request['body']['userId'],
     'recipeId' => $request['body']['recipeId'],
     'recipeName' => $request['body']['recipeName'],
     'recipeImage' => $request['body']['recipeImage']
-
   ];
   send($response);
-};
+}
+
+function add_favorite_recipe($link, $recipe_id, $name, $image){
+  $sql = "INSERT INTO `favoriteProduce`
+  (`userId`,`recipeId`,`name`,`image`)
+  VALUES
+  ({$_SESSION['user_id']}, $recipe_id, $name, $image)";
+  mysqli_query($link, $sql);
+}
