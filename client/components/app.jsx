4cfb09bb1f62ff4b94bc5e0f030ae518e3c5bb1e
Doctorbingbing <christianparizeau@gmail.com>
2019-12-06 00:16:45 +0000

    removed commented code in app.jsx

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -22,14 +22,6 @@ class App extends React.Component {
     };
   }
 
-  componentDidMount() {
-    // fetch('/api/health-check')
-    // .then(res => res.json())
-    // .then(data => this.setState({ message: data.message || data.error }))
-    // .catch(err => this.setState({ message: err.message }))
-    // .finally(() => this.setState({ isTesting: false }));
-  }
-
   render() {
     return (
       <Router>
