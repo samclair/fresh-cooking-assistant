1d79072db4c0c0cede7534d909897f5f6b4aec1c
kittenbites <sammie.nagle@gmail.com>
2019-12-05 16:44:56 -0800

    Tim Gunn'd it

--- server/api/favorite-recipes.php
+++ server/api/favorite-recipes.php
@@ -59,7 +59,7 @@ function unfavorite_recipe($link,$recipe_id){
 }
 
 function get_all_favorites($link){
-  $sql = "SELECT (`name`, `image`, `recipeId`)
+  $sql = "SELECT `name`, `image`, `recipeId`
   FROM `favoriteRecipes`
   WHERE `favoriteRecipe`.`userId` {$_SESSION['user_id']}";
   $result = mysqli_query($link, $sql);
