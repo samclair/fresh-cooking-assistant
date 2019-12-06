00454b64c66f7cd6aa7406dcac8da0da0475d6e1
Doctorbingbing <christianparizeau@gmail.com>
2019-12-06 00:46:11 +0000

    allowed for conditional rendering of favorite star badge fetch request updated to conform to new backend response

--- client/components/recipe-page.jsx
+++ client/components/recipe-page.jsx
@@ -4,29 +4,63 @@ import Ingredient from './ingredient';
 class RecipePage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { recipe: null };
+    this.state = {
+      recipe: null,
+      isFavorite: false
+    };
     this.id = props.match.params.id;
   }
 
   getDetails() {
     fetch(`/api/recipe-details?recipeId=${this.id}`)
       .then(res => res.json())
-      .then(recipe => this.setState({ recipe }))
+      .then(({ details, isFavorite }) => {
+        this.setState({ details, isFavorite });
+      })
       .catch(error => console.error(error.message));
-
   }
 
   componentDidMount() {
     this.getDetails();
   }
 
+  setFavorite() {
+    let reqs = {};
+    if (this.state.isFavorite) {
+      reqs = {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: {
+          recipeId: this.id
+        }
+      };
+    } else {
+      reqs = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: {
+          image: this.state.recipe.image,
+          name: this.state.recipe.name,
+          recipeId: this.id
+        }
+      };
+    }
+    fetch('/api/favorite-recipes', reqs);
+  }
+
   render() {
     if (!this.state.recipe) return null;
+    const favoriteStar = this.state.isFavorite ? <i className="fas fa-star"></i> : <i className="far fa-star"></i>;
     const { image, name, servings, ingredients, instructions } = this.state.recipe;
     const style = { backgroundImage: `url(${image})` };
     return (
       <div>
         <div className='header' style={style} />
+        <div onClick={this.setFavorite}>{favoriteStar}<h3>Favorite</h3></div>
         <div className="container">
           <p className='green text-center font-rubik my-3'>{name}</p>
           <p className='text-center font-rubik mb-2'>{servings}</p>
