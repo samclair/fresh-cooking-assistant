54a8a657f22a308033c53e5d3d6040242d87bac8
kittenbites <sammie.nagle@gmail.com>
2019-12-06 13:23:10 -0800

    Prepared sql statements in favorite recipes endpoint

--- server/api/favorite-recipes.php
+++ server/api/favorite-recipes.php
@@ -36,32 +36,45 @@ if ($request['method'] === 'GET') {
   send($response);
 }
 
-function add_favorite_recipe($link, $recipe_id, $name, $image)
-{
-  error_log($recipe_id);
-  $sql = "INSERT INTO
-  `favoriteRecipes`
-    (`id`,`userId`,`recipeId`,`name`,`image`)
-  VALUES
-    (NULL, {$_SESSION['user_id']}, $recipe_id, '$name', '$image')";
-  mysqli_query($link, $sql);
+function add_favorite_recipe($link, $recipe_id, $name, $image){
+  if(!isset($_SESSION['user_id'])){
+    return false;
+  }
+  $sql = "
+    INSERT INTO
+    `favoriteRecipes`
+      (`id`,`userId`,`recipeId`,`name`,`image`)
+    VALUES
+      (NULL, ?, ?, ?, ?)";
+  $stmt = mysqli_prepare($link, $sql);
+  mysqli_stmt_bind_param($stmt, 'ddss', $_SESSION['user_id'], $recipe_id, $name, $image );
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
   return true;
 }
 
-function unfavorite_recipe($link, $recipe_id)
-{
-  $sql = "DELETE FROM `favoriteRecipes`
-  WHERE `favoriteRecipes`.`recipeId` = $recipe_id
-  AND `favoriteRecipes`.`userId` = {$_SESSION['user_id']}";
-  mysqli_query($link, $sql);
+function unfavorite_recipe($link, $recipe_id){
+  $sql = "
+    DELETE FROM `favoriteRecipes`
+    WHERE `favoriteRecipes`.`recipeId` = ?
+    AND `favoriteRecipes`.`userId` = ?";
+  $stmt = mysqli_prepare($link, $sql);
+  mysqli_stmt_bind_param($stmt, 'dd', $recipe_id, $_SESSION['user_id']);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
   return false;
 }
 
-function get_all_favorites($link)
-{
-  $sql = "SELECT `name`, `image`, `recipeId`
-  FROM `favoriteRecipes`
-  WHERE `favoriteRecipes`.`userId` = {$_SESSION['user_id']}";
-  $result = mysqli_query($link, $sql);
-  return mysqli_fetch_all($result, MYSQLI_ASSOC);
+function get_all_favorites($link){
+  $sql = "
+    SELECT `name`, `image`, `recipeId`
+    FROM `favoriteRecipes`
+    WHERE `favoriteRecipes`.`userId` = ?";
+  $stmt = mysqli_prepare($link, $sql);
+  mysqli_stmt_bind_param($stmt, 'd', $_SESSION['user_id']);
+  mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
+  $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  mysqli_stmt_close($stmt);
+  return $data;
 }
