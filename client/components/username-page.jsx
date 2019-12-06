ab46334f2aa272b2b9c667b4ead1ad11d7dc59ce
Doctorbingbing <christianparizeau@gmail.com>
2019-12-06 22:54:03 +0000

    added create account button on the username page

--- client/components/username-page.jsx
+++ client/components/username-page.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Username extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: ''
+      userName: '',
+      isSignedIn: false
     };
     this.fieldChange = this.fieldChange.bind(this);
     this.getUser = this.getUser.bind(this);
+    this.createUser = this.createUser.bind(this);
   }
 
   getUser(e) {
@@ -19,6 +22,11 @@ class Username extends React.Component {
       .catch(this.setState({ response: true }));
   }
 
+  createUser(e) {
+    e.preventDefault();
+    fetch('api/log-in');
+  }
+
   fieldChange(e) {
     const toChange = {};
     toChange[e.target.name] = e.target.value;
@@ -26,9 +34,12 @@ class Username extends React.Component {
   }
 
   render() {
-    const style = { backgroundColor: '#13A75F', height: '100vh' };
+    if (this.state.isSignedIn) return <Redirect to='/' />;
+    const style = {
+      backgroundColor: '#13A75F', height: '100vh'
+    };
     return (
-      <div className="container pt-4 position-fixed" style={style}>
+      <div className="container pt-4 position-fixed" style={style} >
         <div className="pl-4 my-4">
           <h1 className='font-rubik white font-weight-bold'>FRESH!</h1>
           <h3 className='white'>your</h3>
@@ -36,18 +47,23 @@ class Username extends React.Component {
           <h3 className='white'>assistant</h3>
         </div>
         <form onSubmit={this.getUser}>
-          <div className="form-group d-flex flex-column align-items-center">
+          <div className="form-group d-flex flex-column align-items-center mb-0">
             <input type="text"
               className="form-control w-75 mt-3"
               placeholder='Username'
               name="userName"
               onChange={this.fieldChange}
               value={this.state.userName} />
-            <button className='badge badge-warning white font-weight-bold shadow w-75 mt-2'>Remember Me!</button>
+            <button className='badge badge-warning white font-weight-bold shadow w-75 mt-2'>Sign In</button>
+          </div>
+        </form>
+        <form onSubmit={this.createUser}>
+          <div className="form-group d-flex flex-column align-items-center mt-2">
+            <button className='badge badge-info white font-weight-bold shadow w-75 lightcoral'>Create an Account</button>
           </div>
         </form>
         <h6 className='text-center text-muted '>Enter a username to save recipes</h6>
-      </div>
+      </div >
     );
   }
 }
