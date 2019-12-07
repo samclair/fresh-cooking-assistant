d31c66349f52da70b5672e638828c1a391589bb4
kittenbites <sammie.nagle@gmail.com>
2019-12-07 12:15:31 -0800

    Endpoint can now send back all items in a users fresh list

--- server/api/fresh-list.php
+++ server/api/fresh-list.php
@@ -1,7 +1,14 @@
 <?php
 
+$link = get_db_link();
+
+if(!isset($_SESSION['user_id'])){
+    throw new ApiError('User is not logged in.',400);
+  }
+
 if($request['method'] === 'GET'){
-  get_all_list_items();
+  $response['body'] = get_all_list_items($link);
+  send($response);
 }
 
 if ($request['method'] === 'POST') {
@@ -16,8 +23,18 @@ if ($request['method'] === 'PATCH') {
   edit_list_item();
 }
 
-function get_all_list_items(){
-  return;
+function get_all_list_items($link){
+  $sql = "
+    SELECT `name`, `isComplete`
+    FROM `favoriteProduceItems`
+    WHERE `favoriteProduceItems`.`userId` = ?";
+  $stmt = mysqli_prepare($link, $sql);
+  mysqli_stmt_bind_param($stmt, 'd', $_SESSION['user_id']);
+  mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
+  $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  mysqli_stmt_close($stmt);
+  return $data;
 }
 
 function add_list_item(){
