088823ae72d5423b7df06293d2ce25da429c13f2
Doctorbingbing <christianparizeau@gmail.com>
2019-12-07 19:57:19 +0000

    added methods to make fetch calls to back end

--- client/components/fresh-list.jsx
+++ client/components/fresh-list.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import ListItem from './list-items';
+import Ingredient from './ingredient';
 class FreshList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       listItems: [1, 2, 3, 4, 5]
     };
+    this.removeAllProduce = this.removeAllProduce.bind(this);
+    this.removeProduceItem = this.removeProduceItem.bind(this);
   }
 
   componentDidMount() {
     // get list from backend
+    this.getUserList();
   }
 
   getUserList() {
     // do the thing
   }
 
+  removeProduceItem() {
+    // send delete request with string
+  }
+
+  removeAllProduce() {
+    // send delete request with no body
+  }
+
   render() {
 
-    const items = this.state.listItems.map((element, index) => {
-      return <ListItem key={index} text={element} />;
+    const items = this.state.listItems.map((item, index) => {
+      return <Ingredient key={index} item={item} />;
     });
 
     return (
--- client/components/list-items.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ListItem(props) {
-  return <h1>{props.text}</h1>;
-}
-
-export default ListItem;
