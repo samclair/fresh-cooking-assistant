558670ad718ab7f1f59a6d31eee2d3a40e4e8012
Doctorbingbing <christianparizeau@gmail.com>
2019-12-07 20:25:10 +0000

    setup basic methods to make requests from backend and added button to clear all

--- client/components/fresh-list.jsx
+++ client/components/fresh-list.jsx
@@ -4,7 +4,7 @@ class FreshList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      listItems: [1, 2, 3, 4, 5]
+      listItems: []
     };
     this.removeAllProduce = this.removeAllProduce.bind(this);
     this.removeProduceItem = this.removeProduceItem.bind(this);
@@ -12,10 +12,13 @@ class FreshList extends React.Component {
 
   componentDidMount() {
     // get list from backend
-    this.getUserList();
+    // this.getUserList();
   }
 
   getUserList() {
+    fetch('/api/fresh-list')
+      .then(res => res.json())
+      .then(listItems => this.setState({ listItems }));
     // do the thing
   }
 
@@ -23,20 +26,33 @@ class FreshList extends React.Component {
     // send delete request with string
   }
 
-  removeAllProduce() {
+  removeAllProduce(e) {
+    e.preventDefault();
     // send delete request with no body
   }
 
   render() {
 
     const items = this.state.listItems.map((item, index) => {
-      return <Ingredient key={index} item={item} />;
+
+      return (
+        <div key={index}>
+          <i className='far fa-circle' />
+          <Ingredient measurement={item.measurement} isInDatabase={item.isInDatabase} />
+        </div>);
     });
 
     return (
       <div className="container">
         <h1 className="green font-rubik text-center my-4">Fresh List!</h1>
-        <ul>{items}</ul>
+        <div className="d-flex flex-column">
+          <ul>{items}</ul>
+          <form className="align-self-center" onSubmit={this.removeAllProduce}>
+            <button className="btn primary-label">
+              Clear All
+            </button>
+          </form>
+        </div>
       </div>);
   }
 }
