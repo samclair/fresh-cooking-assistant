a1bf40a3c8b8838611b77ce8bb089946d8128271
Christian Haney <christianhaney1996@gmail.com>
2019-12-09 12:00:13 -0800

    Recipe Card Component modified FavoriteRecipePage component to make use of new RecipeCard component

--- client/components/recipe-card.jsx
+++ client/components/recipe-card.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 function RecipeCard(props) {
   return (
-    <div className='d-flex flex-column col-6' key={props.recipe.id}>
+    <div className='d-flex flex-column col-6' key={props.recipe.recipeId}>
       <Link to={`/recipes/${props.recipe.id}`}>
         <img
           className='featured-produce-image shadow-sm'
-          src={props.recipe.thumbnail}
+          src={props.recipe.image}
           alt={props.recipe.name}
         />
         <p><u>{props.recipe.name}</u></p>
