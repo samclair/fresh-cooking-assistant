edb0285d3584b0bec92800c2b10ea6323d15551d
kittenbites <sammie.nagle@gmail.com>
2019-12-09 15:05:49 -0800

    Updated produce-in-season to respond with a list of all produce in the database if no season query in GET request

--- server/api/produce-in-season.php
+++ server/api/produce-in-season.php
@@ -4,7 +4,7 @@ include_once '_helpers.php';
 $link = get_db_link();
 
 if ($request['method'] === 'GET') {
-  if(!isset($request['query']['seasonName'])) { throw new ApiError('Season name required.', 400); }
+  if(isset($request['query']['seasonName'])) {
   $season_name = $request['query']['seasonName'];
   $season_id = get_season_id($link, $season_name);
   $produce_list = get_produce_list($link, $season_id);
@@ -12,5 +12,26 @@ if ($request['method'] === 'GET') {
   $response['body'] = [ 'produceList' => $produce_list ];
   if ($season_id === $current_season) { $response['body']['isCurrentSeason'] = true; }
   else { $response['body']['isCurrentSeason'] = false; }
+  } else{
+    $produce_list = get_all_produce($link);
+    $response['body'] = ['produceList' => $produce_list];
+  }
   send($response);
+}
+
+function get_all_produce($link){
+  $sql = "
+    SELECT DISTINCT `id`, `name`, `produceImg`
+    FROM `produce`
+    ";
+  $stmt = mysqli_prepare($link, $sql);
+  mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
+  if (!mysqli_num_rows($result)) {
+    $produce = [];
+  } else {
+    $produce = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  }
+  mysqli_stmt_close($stmt);
+  return $produce;
 }
