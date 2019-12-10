adca85c7ce1d24c40301f4edf32b3994cc954295
Christian Haney <christianhaney1996@gmail.com>
2019-12-09 20:02:19 -0800

    GET requests now return the names and addresses of farmer's markets near the provided location ranked by distance

--- server/api/maps-list.php
+++ server/api/maps-list.php
@@ -6,24 +6,31 @@ if ($request['method'] === "GET") {
   $location;
   if (!isset($request['query']['location'])) { $location = '32.956423,-117.040781'; }
   else { $location = $request['query']['location']; }
-  $results = get_farmers_market_list($maps_api_key, $location);
-  $response['body'] = $results;
+  $data = get_farmers_market_list($maps_api_key, $location);
+  $response['body'] = [];
+  foreach ($data -> results as $place) {
+    array_push($response['body'], [
+      'name' => $place -> name,
+      'address' => $place -> vicinity
+    ]);
+  }
   send($response);
 }
 
 function get_farmers_market_list($maps_api_key, $location) {
   $ch = curl_init();
   $options = [
-    CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
-      ."key=$maps_api_key&locationbias=circle:25000@$location&query=farmers+market",
+    CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+      ."?key=$maps_api_key&location=$location&rankby=distance&"
+      ."&name=farmers+market",
     CURLOPT_HTTPGET => true,
     CURLOPT_CONNECTTIMEOUT => 10,
     CURLOPT_TIMEOUT => 10,
     CURLOPT_RETURNTRANSFER => true
   ];
   curl_setopt_array($ch,$options);
-  $results = curl_exec($ch);
+  $data = curl_exec($ch);
   $error = curl_error($ch);
   curl_close($ch);
-  return $error ? $error : json_decode($results);
+  return $error ? $error : json_decode($data);
 }
