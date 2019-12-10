8c02c9db7418d9cb05c68e20c0852e35de5ae23e
Christian Haney <christianhaney1996@gmail.com>
2019-12-09 16:58:11 -0800

    Renamed maps.php endpoint to maps-list.php

--- server/api/maps-list.php
+++ server/api/maps-list.php
@@ -4,11 +4,11 @@ require_once '_api-keys.php';
 
 if ($request['method'] === "GET") { }
 
-function get_farmers_market_details($maps_key)
-{
+function get_farmers_market_details($maps_key, $location) {
   $ch = curl_init();
   $options = [
-    CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/textsearch/json?key=$maps_key&query=farmer's+market",
+    CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
+      ."key=$maps_key&location=$location&query=farmer's+market",
     CURLOPT_HTTPGET => true,
     CURLOPT_CONNECTTIMEOUT => 10,
     CURLOPT_TIMEOUT => 10,
@@ -18,5 +18,5 @@ function get_farmers_market_details($maps_key)
   $results = curl_exec($ch);
   $error = curl_exec($ch);
   curl_close($ch);
-  return $error ? $error : json_decode($results)
+  return $error ? $error : json_decode($results);
 }
