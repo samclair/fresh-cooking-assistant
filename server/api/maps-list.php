69f8f7eab42ceeb0d9fa54f4db1f5e9b4e0d9295
Christian Haney <christianhaney1996@gmail.com>
2019-12-09 17:51:27 -0800

    Maps list endpoint now accepts get requests

--- server/api/maps-list.php
+++ server/api/maps-list.php
@@ -2,13 +2,20 @@
 
 require_once '_api-keys.php';
 
-if ($request['method'] === "GET") { }
+if ($request['method'] === "GET") {
+  $location;
+  if (!isset($request['query']['location'])) { $location = '32.956423,-117.040781'; }
+  else { $location = $request['query']['location']; }
+  $results = get_farmers_market_list($maps_api_key, $location);
+  $response['body'] = $results;
+  send($response);
+}
 
-function get_farmers_market_details($maps_key, $location) {
+function get_farmers_market_list($maps_api_key, $location) {
   $ch = curl_init();
   $options = [
     CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
-      ."key=$maps_key&location=$location&query=farmer's+market",
+      ."key=$maps_api_key&locationbias=circle:25000@$location&query=farmers+market",
     CURLOPT_HTTPGET => true,
     CURLOPT_CONNECTTIMEOUT => 10,
     CURLOPT_TIMEOUT => 10,
@@ -16,7 +23,7 @@ function get_farmers_market_details($maps_key, $location) {
   ];
   curl_setopt_array($ch,$options);
   $results = curl_exec($ch);
-  $error = curl_exec($ch);
+  $error = curl_error($ch);
   curl_close($ch);
   return $error ? $error : json_decode($results);
 }
--- server/public/index.php
+++ server/public/index.php
@@ -4,6 +4,7 @@ require_once '../api/_lifecycle.php';
 switch ($request['path']) {
   case '/api/log-in':
   case '/api/seasons':
+  case '/api/maps-list':
   case '/api/fresh-list':
   case '/api/recipe-list':
   case '/api/health-check':
@@ -12,7 +13,6 @@ switch ($request['path']) {
   case '/api/produce-details':
   case '/api/favorite-recipes':
   case '/api/produce-in-season':
-  case '/api/maps':
     require_once "..${request['path']}.php";
   default:
     throw new ApiError("Cannot ${request['method']} ${request['path']}", 404);
