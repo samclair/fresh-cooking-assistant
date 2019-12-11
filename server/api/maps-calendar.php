55dc91fc01d31226c6ae5fe19279c7f86cef9d4e
Christian Haney <christianhaney1996@gmail.com>
2019-12-10 16:07:20 -0800

    refactored format_dates function to make use of php date function

--- server/api/maps-calendar.php
+++ server/api/maps-calendar.php
@@ -27,16 +27,10 @@ function format_dates($dates) {
   $day = explode(': ', $dates)[0];
   $times = explode(': ', $dates)[1];
   $open = explode(' - ', $times)[0];
-  $open = date(DATE_ISO8601, strtotime("next $day $open"));
-  $open_date = explode('T', $open)[0];
-  $open_date = str_replace('-', '', $open_date);
-  $open_time = explode('T', $open)[1];
-  $open_time = str_replace(':', '', explode('-',$open_time)[0]);
+  $open_formatted = date('Ymd', strtotime("next $day $open"));
+  $open_formatted .= 'T'.date('His', strtotime("next $day $open")).'Z';
   $close = explode(' - ', $times)[1];
-  $close = date(DATE_ISO8601, strtotime("next $day $close"));
-  $close_date = explode('T', $close)[0];
-  $close_date = str_replace('-', '', $close_date);
-  $close_time = explode('T', $close)[1];
-  $close_time = str_replace(':', '', explode('-',$close_time)[0]);
-  return urlencode("{$open_date}T{$open_time}Z/{$close_date}T{$close_time}Z");
+  $close_formatted = date('Ymd', strtotime("next $day $close"));
+  $close_formatted .= 'T'.date('His', strtotime("next $day $close")).'Z';
+  return urlencode("$open_formatted/$close_formatted");
 }
